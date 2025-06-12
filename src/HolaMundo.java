de629f8ee52fa38c9549734bb886829475d7ca12
Jose l. Azagra <azagramac@gmail.com>
2025-06-12 09:03:29 +0200

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,8 +1,20 @@
 package blog.azagra.dev.holamundo;
+
 import blog.azagra.dev.mensajes.Mensaje;
+import blog.azagra.dev.despedida.Despedida;
 
 public class HolaMundo {
-  public String saludar() {
-    return Mensaje.saludar();
-  }
+    public String saludar() {
+        return Mensaje.saludar();
+    }
+
+    public String despedirse() {
+        return Despedida.saludar();
+    }
+
+    public static void main(String[] args) {
+        HolaMundo hm = new HolaMundo();
+        System.out.println(hm.saludar());
+        System.out.println(hm.despedirse());
+    }
 }
